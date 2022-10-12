d31385825ec5f99e361b5d2049cd9dd4013f91bb
Mukesh Mahato <mukeshmahato843@gmail.com>
2022-10-12 21:10:00 +0545

    Adding Copy Button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.getElementById('body');
 const text = document.getElementById('title');
 const btn = document.querySelector(".button")
+const copyBtn = document.getElementById("copy");
 btn.addEventListener("click", () => {
     const hexContent = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
     var i, colorCode = "#", random;
@@ -10,4 +11,22 @@ btn.addEventListener("click", () => {
         colorCode += hexContent[random];
     }
     body.style.background = colorCode;
-    text.innerHTML = "Current background color is <br>" + colorCode;})
+    text.innerHTML = colorCode;
+})
+
+copyBtn.addEventListener("click", () => {
+    console.log("clicked");
+    let tempInput = document.createElement("input");
+    tempInput.style.display = "none";
+    body.append(tempInput);
+    tempInput.value = text.innerText
+    tempInput.focus();
+    tempInput.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(tempInput.value);
+    body.removeChild(tempInput);
+    document.getElementById("alert").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("alert").style.display = "none";
+
+    }, 500);
+})
